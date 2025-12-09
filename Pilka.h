a87c3340e1daa1bc57f50c071a5fbbd676621fd7
Wiktorr <wdorakosv2@gmail.com>
2025-12-10 00:47:14 +0100

    Końcowy kod gry

--- Pilka.h
+++ Pilka.h
@@ -1,8 +1,8 @@
-﻿#pragma once
+#pragma once
 #include <SFML/Graphics.hpp>
 #include <cmath> 
 #include "Paletka.h" 
-#include "Paletka.h"
+#include "bloki.h"
 
 class Pilka {
 private:
@@ -47,12 +47,13 @@ public:
 			shape.setPosition({ x, y - 3 });
 		}
 	};
+
 	bool collidePaddle(const Paletka& p)
 	{
-
 		if (p.getX() - p.getSzerokosc() / 2 <= x && y + promien >= p.getY() - p.getWysokosc() / 2 && y - promien < p.getY() - p.getWysokosc() / 2 && (p.getX() + p.getSzerokosc() / 2 >= x)) {
 
 			predkoscY = -std::abs(predkoscY);
+			predkoscX = (x - p.getX()) / (p.getSzerokosc() / 2) * 4.f;
 			y = p.getY() - p.getWysokosc() / 2 - promien;
 			shape.setPosition({ x, y });
 			return true;
@@ -62,12 +63,62 @@ public:
 	};
 	void draw(sf::RenderTarget& target) {
 		target.draw(shape);
- 	};
+	};
 	float getX() const { return x; }
 	float getY() const { return y; }
 	float getPromien() const { return promien; }
 	float getPredkoscX() const { return predkoscX; }
 	float getPredkoscY() const { return predkoscY; }
+	sf::FloatRect getBounds() const {
+		return shape.getGlobalBounds(); 
+	}
 
+	bool sprawdzKolizjeBloku(blok& b)
+	{
+		
+		if (!b.czyAktywny())
+			return false;
+		
+
+		sf::FloatRect pilkaBounds = getBounds();
+		sf::FloatRect klocekBounds = b.getBounds();
+
+		std::optional<sf::FloatRect> overlap = pilkaBounds.findIntersection(klocekBounds); 
+
+			if (overlap.has_value()) {  
+				sf::FloatRect overlapWartosc = overlap.value(); 
+
+				b.uderz();
+
+				if (overlapWartosc.size.x < overlapWartosc.size.y)
+				{
+					bounceX();
+				
+					if (pilkaBounds.position.x < klocekBounds.position.x)
+						x -= overlapWartosc.size.x; 
+					else
+						x += overlapWartosc.size.x; 
+				}
+				else
+				{
+					bounceY();
+				
+					if (pilkaBounds.position.y < klocekBounds.position.y)
+						y -= overlapWartosc.size.y; 
+					else
+						y += overlapWartosc.size.y; 
+				}
+			}
+			else
+				return false;
+		
+		shape.setPosition({ x, y });
+
+		return true;
+	}
 };
+
+
+
+
 
